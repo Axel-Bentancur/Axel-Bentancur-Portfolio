16e71d7673f33535cdc881fa4b6808e508d93f5d
axel <axel-bentancur@hotmail.com>
2020-11-02 21:26:16 -0300

    Updates

---
+++ precache-manifest.a72763d5965a7b4b5af1d256f0d045c8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d4ccf93c7b4006a228171ebb64616d",
+    "url": "/Axel-Bentancur-Portfolio/index.html"
+  },
+  {
+    "revision": "28b7a2145994ad593266",
+    "url": "/Axel-Bentancur-Portfolio/static/css/main.fb2af008.chunk.css"
+  },
+  {
+    "revision": "f14ce4a22a65488d4bf8",
+    "url": "/Axel-Bentancur-Portfolio/static/js/2.8505b853.chunk.js"
+  },
+  {
+    "revision": "d4b0302a1e882c57090284dfea2c6f01",
+    "url": "/Axel-Bentancur-Portfolio/static/js/2.8505b853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b7a2145994ad593266",
+    "url": "/Axel-Bentancur-Portfolio/static/js/main.389844b4.chunk.js"
+  },
+  {
+    "revision": "37858a74157ad6c53bff",
+    "url": "/Axel-Bentancur-Portfolio/static/js/runtime-main.ad9832ff.js"
+  },
+  {
+    "revision": "c09c76965d163d012dad1e8c64d52aa4",
+    "url": "/Axel-Bentancur-Portfolio/static/media/asd2.c09c7696.jpg"
+  },
+  {
+    "revision": "e13dd10ee0a5b7d5ba72679fd0505b61",
+    "url": "/Axel-Bentancur-Portfolio/static/media/bootstrap-plain.e13dd10e.svg"
+  },
+  {
+    "revision": "4bc37d5a3fa7a80854f173ae28045a4e",
+    "url": "/Axel-Bentancur-Portfolio/static/media/css3-plain.4bc37d5a.svg"
+  },
+  {
+    "revision": "ed844b5cd9c5a04e97770033cc5dfeee",
+    "url": "/Axel-Bentancur-Portfolio/static/media/external-link.ed844b5c.svg"
+  },
+  {
+    "revision": "90eaa97fd6d88b2675839db20153c668",
+    "url": "/Axel-Bentancur-Portfolio/static/media/git-original.90eaa97f.svg"
+  },
+  {
+    "revision": "514506bdc3ec9b72966024bb3246b99f",
+    "url": "/Axel-Bentancur-Portfolio/static/media/github-original.514506bd.svg"
+  },
+  {
+    "revision": "2e13fa37cd018522b9261d8a1051539e",
+    "url": "/Axel-Bentancur-Portfolio/static/media/html5-plain.2e13fa37.svg"
+  },
+  {
+    "revision": "f13153bb4d5b60b24f95c0bcbe5cae03",
+    "url": "/Axel-Bentancur-Portfolio/static/media/javascript-plain.f13153bb.svg"
+  },
+  {
+    "revision": "557343ec2b7bcc3e93496218a2937555",
+    "url": "/Axel-Bentancur-Portfolio/static/media/jquery-plain.557343ec.svg"
+  },
+  {
+    "revision": "d3ee64f6a13442a3d5dd78202b65b1e7",
+    "url": "/Axel-Bentancur-Portfolio/static/media/linkedin-original.d3ee64f6.svg"
+  },
+  {
+    "revision": "1d2471fadd39939c746b12d1136b7cf0",
+    "url": "/Axel-Bentancur-Portfolio/static/media/mongodb-plain.1d2471fa.svg"
+  },
+  {
+    "revision": "cdfad59f12e7557c3b63d4dadb42349c",
+    "url": "/Axel-Bentancur-Portfolio/static/media/nodejs-plain.cdfad59f.svg"
+  },
+  {
+    "revision": "a5da7c5468e7548c493d84a2fe981a76",
+    "url": "/Axel-Bentancur-Portfolio/static/media/pdf.a5da7c54.svg"
+  },
+  {
+    "revision": "4f61fea6b9766f88b8e31472f35ac209",
+    "url": "/Axel-Bentancur-Portfolio/static/media/photoshop-plain.4f61fea6.svg"
+  },
+  {
+    "revision": "600b0d29f1aec4da2271feac74966d9b",
+    "url": "/Axel-Bentancur-Portfolio/static/media/react-original.600b0d29.svg"
+  },
+  {
+    "revision": "98425b2af4530a0f409306f8de09846d",
+    "url": "/Axel-Bentancur-Portfolio/static/media/sass-original.98425b2a.svg"
+  }
+]);
